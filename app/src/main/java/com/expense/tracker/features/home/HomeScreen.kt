369e07409f96ec471b3e98200723eac1a3343140
Fiantso Harena <fiantsorav@gmail.com>
2026-04-27 22:03:48 +0300

    style(home): add TopAppBar and FloatingActionButton to HomeScreen

--- app/src/main/java/com/expense/tracker/features/home/HomeScreen.kt
+++ app/src/main/java/com/expense/tracker/features/home/HomeScreen.kt
@@ -7,14 +7,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import java.time.YearMonth
@@ -30,7 +39,25 @@ fun HomeScreen(
 ) {
     var currentMonth by remember { mutableStateOf(YearMonth.now()) }
 
-    Scaffold() { paddingValues ->
+    Scaffold(
+        topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Volako", style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.Bold
+                )
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.background
+            )
+        )
+    }, floatingActionButton = {
+        FloatingActionButton(
+            onClick = onNavigateToAddExpense, containerColor = MaterialTheme.colorScheme.primary
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Add Expense")
+        }
+    }, modifier = modifier
+    ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
